90d6d70b7bff3299490504110db2fef481415dfa
lej4545 <lej4545@naver.com>
2021-08-13 23:16:11 +0900

    second commit

--- DP/Effective_money.py
+++ DP/Effective_money.py
@@ -1,15 +1,20 @@
+# 정수 n, m을 입력 받기
 n, m = list(map(int, input().split()))
-d = [0] + [10001] * (m+1)
+
+# n개의 화폐 단위 정보를 입력받기
 a = [0]* n
 for i in range(n):
     a[i] = int(input())
 
+# 한 번 계산된 결과를 저장하기 위한 DP 테이블 초기화
+d = [0] + [10001] * (m+1)
+
 for i in range(n):
     for j in range(a[i], m+1):
-        if d[j-a[i]] != 10001:
+        if d[j-a[i]] != 10001: # i-k원을 만드는 방법이 존재하는 경우
             d[j] = min(d[j], d[j-a[i]] + 1)
-
-if d[m] == 10001:
+# 계산된 결과 출력
+if d[m] == 10001: # 최종적으로 m원을 만드는 방법이 없는 경우
     print(-1)
 else:
     print(d[m])
