b79b02ef329f9c7fc0775e3958b4ac8aa6c08607
lej4545 <lej4545@naver.com>
2021-08-14 23:19:42 +0900

    for문 공부

---
+++ For/quick_A+B.py
@@ -0,0 +1,10 @@
+import sys
+
+case = int(input())
+for i in range(case):
+    a, b = map(int, sys.stdin.readline())
+    print(a + b)
+
+num = int(sys.stdin.readline())
+for i in range(1, num+1):
+    print(i)
