67592ef93e6cc0d5c6a6ee9a643588bdcd8b70bf
lej4545 <lej4545@naver.com>
2021-08-29 23:19:08 +0900

    수학 문제2

--- 수학 문제/베르트랑 공준.py
+++ 수학 문제/베르트랑 공준.py
@@ -1,11 +1,15 @@
-while :
-    m, n = map(int, input().split())
-    array = [False, False] + [True] * (n - 1)
-    primes = []
-    for i in range(2, n+1):
-      if array[i] is True:
-        if i >= m:
-            print(i)
-        for j in range(2*i, n+1, i):
-            array[j] = False
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    else:
+        array = [False, False] + [True] * (2*n - 1)
+        primes = []
+        for i in range(2, 2*n+1):
+          if array[i] is True:
+            if n < i <= 2 * n:
+                primes.append(i)
+            for j in range(2*i, 2*n+1, i):
+                array[j] = False
+        print(len(primes))
 
--- 수학 문제/소인수분해.py
+++ 수학 문제/소인수분해.py
@@ -1,1 +1,14 @@
-w
+a = int(input())
+lst = []
+def prime(n):
+    for i in range(2, (n/2) + 1):
+        if n % i == 0:
+            lst.append(i)
+            print(i)
+            prime(n // i)
+            break
+prime(a)
+
+if len(lst) == 0:
+    if a != 1:
+        print(a)
