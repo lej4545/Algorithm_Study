d88ece47cba860acc5d802159b3af635f0dd70b9
lej4545 <lej4545@naver.com>
2021-09-06 23:01:41 +0900

    순열 문제

--- 완전검색-순열조합/모든 순열.py
+++ 완전검색-순열조합/모든 순열.py
@@ -1,22 +1,9 @@
-def dfs_permutation(array, r):
-    i_array = [(x, i) for i, x in enumerate(array)]
-    print(i_array)
-    stack = [[i] for _, i in i_array]
-    return_list = []
+from itertools import permutations
 
-    while len(stack) > 0:
-        current = stack.pop(0)
-        for i in range(len(i_array)): # 4번만큼 반복
-            if i not in current:
-                temp = current + [i_array[i][1]]
-                if len(temp) == r:
-                    elements = []
-                    for idx in temp:
-                        elements.append(i_array[idx][0])
-                    return_list.extend([elements])
-                else:
-                    stack.append(temp)
-    return return_list
+n = int(input())
+data = list(range(1, n+1))
 
-
-print(dfs_permutation("ABCD", 3))
+for i in permutations(data):
+    for j in i:
+        print(j, end=' ')
+    print()
